bdff1f071bd877d1eaa48f8f87ca0f729e4986f2
JunkyuLee <ljk9707@naver.com>
2023-06-09 01:23:05 +0900

    select_fruit_py 오타 수정

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import pendulum
 import datetime
-from airflow.operators.empty import BashOperator
+from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_select_fruit",
