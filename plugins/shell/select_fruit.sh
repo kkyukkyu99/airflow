d04adcbef84299f92d5e333647826c2f891a8882
ljk9707 <ljk9707@naver.com>
2023-06-11 15:22:09 +0900

    select_fruit.sh 2차 시도

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -3,7 +3,7 @@ if [ $FRUIT == APPLE ];then
 	echo "You selected Apple!"
 elif [ $FRUIT == ORANGE ];then
 	echo "You selected Orange!"
-elif [ $FRUIT == GRAPE ]; then
+elif [ $FRUIT == GRAPE ];then
 	echo "You selected Grape!"
 else
 	echo "You selected other Fruit!"
